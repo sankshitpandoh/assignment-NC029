a97270809bc9a87f86a420516b181159a4abb385
Sankshit Pandoh <sankshit.p@uplers.in>
2020-02-20 18:41:02 +0530

    hero-banner slides added and carousel slider made auto

---
+++ assets/js/general.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+var owl = $('.hero-banner-slider');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+});
